242218918476915c23f89183374e560fcfcc540d
918 <rmarudxo05@gmail.com>
2025-03-24 11:32:55 +0900

    v0.5 move_zeros

--- week03.py
+++ week03.py
@@ -1,9 +1,19 @@
-l = [11, 9, -77, 8]
-for i in eunumerate(l):
-    print(i)
+def move_zeros(a_list):
+    zero_index = 0
+    for index, n in enumerate(a_list):
+        if n != 0:
+            a_list[zero_index] = n
+            if zero_index != index:
+                a_list[index] = 0
+            zero_index += 1
+    return (a_list)
 
-# import array
-#
-# arr = array.array('f',[11, 9, -77, 8])
-# for i in range(len(arr)):
-#     print(f"{arr[i]:3} {id(arr[i])}")
+a_list = [8,0,3,0,12]
+move_zeros(a_list)
+print(a_list)
+
+# index 0 / n 8 / zero_index 1 / a_list [8,0,3,0,12]
+# index 1 / n 0 / zero_index 1 / a_list [8,0,3,0,12]
+# index 2 / n 3 / zero_index 2 / a_list [8,3,0,0,12]
+# index 3 / n 0 / zero_index 2 / a_list [8,3,0,0,12]
+# index 4 / n 12 / zero_index 3 / a_list [8,3,12,0,0]
