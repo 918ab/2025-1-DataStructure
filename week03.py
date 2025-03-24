7a83c0e59bab32a31f70ffb86356f96ee1d744ca
918 <rmarudxo05@gmail.com>
2025-03-24 11:07:49 +0900

    v0.4 array

--- week03.py
+++ week03.py
@@ -1,5 +1,9 @@
-import array
+l = [11, 9, -77, 8]
+for i in eunumerate(l):
+    print(i)
 
-arr = array.array('f',[11, 9, -77, 8])
-for i in range(len(arr)):
-    print(f"{arr[i]:3} {id(arr[i])}")
+# import array
+#
+# arr = array.array('f',[11, 9, -77, 8])
+# for i in range(len(arr)):
+#     print(f"{arr[i]:3} {id(arr[i])}")
