286749d59907276f4d671b1e96bd99afd92aba43
918 <rmarudxo05@gmail.com>
2025-03-24 10:45:17 +0900

    v0.4 array

--- week03.py
+++ week03.py
@@ -1,5 +1,3 @@
 array = [11, 9, -77, 8]
-print(f"{array[0]:3} {id(array[0])}")
-print(f"{array[1]:3} {id(array[1])}")
-print(f"{array[2]:3} {id(array[2])}")
-print(f"{array[3]:3} {id(array[3])}")
+for i in len(array):
+    print(f"{array[i]:3} {id(array[i])}")
