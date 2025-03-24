4f1b2b09d21494c521185534a86ddb6b142b009e
918 <rmarudxo05@gmail.com>
2025-03-24 11:56:41 +0900

    v0.7 append

--- week03.py
+++ week03.py
@@ -1,5 +1,18 @@
-gropus = ['HOT', 'Seventeen', 'Black Pink', 'NJZ']
-ratings = [1, 2, 4, 3]
+def duplicate_city(cities):
+    result = list()
+    s = set()
 
-gropus_rating  = list(zip(gropus,ratings))
-print(gropus_rating)
+    for city in cities:
+        l1 = len(s)
+        s.add(city)
+        l2 = len(s)
+        if l1 == l2:
+            result.append(city)
+    return result
+
+cities = ['Incheon', 'Incheon', 'Incheon', 'Gimpo', 'Seoul', 'Seoul']
+# cities = {'Incheon', 'Incheon', 'Incheon', 'Gimpo', 'Seoul', 'Seoul'}
+cities.append('Anyang')
+cities.append('Seoul')
+print(cities)
+print(set(duplicate_city(cities)))
