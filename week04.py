4578196efd18c5468a066622489b0e3549ce1a77
918 <rmarudxo05@gmail.com>
2025-03-31 11:22:38 +0900

    v1.3 linked list

--- week04.py
+++ week04.py
@@ -15,8 +15,17 @@ class LinkedList:
         while current.link:
             current = current.link #다음노드로 이동
         current.link = Node(data)
-
+    def __str__(self):
+        node = self.head
+        out = ""
+        while node is not None:
+            # print(node.data)
+            out = out +str(node.data) + " -> "
+            node = node.link
+        return out + "end"
 ll = LinkedList()
 ll.append(8)
 ll.append(10)
+ll.append(-9)
+print(ll)
 
