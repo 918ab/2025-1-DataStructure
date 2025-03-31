06bf39161f3a08413081751ef7e20123a43782d1
918 <rmarudxo05@gmail.com>
2025-03-31 10:50:57 +0900

    v0.8 LinkedList

---
+++ week04.py
@@ -0,0 +1,22 @@
+class Node:
+    def __init__(self, data, link=None):
+        self.data = data
+        self.link = link
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self,data):
+        if not self.head: #LinkedList가 노드가 하나도 없는 상태(비어있는 상태)
+            self.head = Node(data) #새노드를 head에 연결
+            return
+        current = self.head
+        while current.link:
+            current = current.link #다음노드로 이동
+        current.link = Node(data)
+
+ll = LinkedList()
+ll.append(8)
+ll.append(10)
+
