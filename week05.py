b2060ed32056aa2b251354aa631abbdf4f17b137
918 <rmarudxo05@gmail.com>
2025-04-07 11:36:30 +0900

    v2.1 Stack

--- week05.py
+++ week05.py
@@ -1,26 +1,27 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.link = None
+
 class Stack:
     def __init__(self):
-        self.items = list()
-    def push(self,item):
-        self.items.append(item)
+        self.top = None
+    def push(self,data):
+        node = Node(data)
+        if self.top is None:
+            self.top = node
+        else:
+            node.link = self.head
+            self.top = node
     def pop(self):
-        return self.items.pop()
-    def size(self):
-        return len(self.items)
-    def is_empty(self):
-        return len(self.items) == 0
-    def peek(self):
-        return self.items[-1]
-
+        if self.top is None:
+            #raise  IndexError("스텍이 비어있습니다")
+            return "Stack is Empty"
+        poped_node = self.top
+        self.top = self.top.link
+        return poped_node.data
 
 s1 = Stack()
-print(s1.is_empty())
-s1.push("자료 구조")
-print(s1.is_empty())
-s1.push("데이터베이스")
-print(s1.size())
-print(s1.peek())
-print(s1.size())
 print(s1.pop())
-print(s1.size())
-print(s1.peek())
+s1.push("자료구조")
+print(s1.pop())
